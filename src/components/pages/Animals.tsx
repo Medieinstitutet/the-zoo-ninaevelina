87d94515fa12b4aff21de2be398cc30cf4d6d333
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-05-02 10:35:07 +0200

    feat: add Animals-component

---
+++ src/components/pages/Animals.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../Navbar";
+import { Link } from "react-router-dom";
+
+export const Animals = () => {
+    return (
+        <>
+        <Navbar></Navbar>
+        <p>Animal-page works</p>
+        </>
+    );
+};
