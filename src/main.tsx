b3a38da51d88a96ea395743a02e0679c728d728e
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-05-02 10:34:46 +0200

    build: set up router and route-paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,55 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+//import App from './App'
 import './index.scss'
+import { Main } from './components/pages/Main';
+import { Animals } from './components/pages/Animals';
 
+/*
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+)*/
+/*
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+*/
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Main></Main>,
+    //errorElement:
+  },
+  {
+    path: "/animals",
+    element: <Animals></Animals>,//animalcomponent 
+  },
+  {
+    path: "/animals/:id",
+    element: "", //single animal
+  },
+]);
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
