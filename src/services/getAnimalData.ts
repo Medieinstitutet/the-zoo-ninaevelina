64fec371c2c1ee84a57f572294e93592d81a0a31
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-05-05 10:44:55 +0200

    fix: format document w. prettier

--- src/services/getAnimalData.ts
+++ src/services/getAnimalData.ts
@@ -1,15 +1,16 @@
 import axios from "axios";
 import { IAnimal } from "../models/IAnimal";
 
-export async function getAnimalData (): Promise<IAnimal[]> {
-    try {
-       const response = await axios.get(`https://animals.azurewebsites.net/api/animals`);
-        console.log(response.data);
-        return response.data;
-    } catch (error) {
-        console.log(error);
-        console.log('some message');
-        return [];
-    }
-
+export async function getAnimalData(): Promise<IAnimal[]> {
+  try {
+    const response = await axios.get(
+      `https://animals.azurewebsites.net/api/animals`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    console.log("some message");
+    return [];
+  }
 }
