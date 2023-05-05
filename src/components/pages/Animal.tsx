7e0bf5e8a772630230a8602d1338a84f569512dd
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-05-05 10:47:15 +0200

    feat: add Animal-component

--- src/components/pages/Animal.tsx
+++ src/components/pages/Animal.tsx
@@ -1,7 +1,24 @@
-export const Animal = () => {
-    return (
-        <>
-        <p>Test</p>
-        </>
-    )
-}
+import { Navbar } from "../Navbar";
+import { useEffect } from "react";
+import { useState } from "react";
+import { IAnimal } from "../../models/IAnimal";
+import { useParams } from "react-router-dom";
+import { getLSData } from "../../services/getLSData";
+
+//  <div key={animal.id}>
+
+export const Animal = (animal: IAnimal) => {
+  return (
+    <>
+      <div>
+        <h1>{animal.name}</h1>
+        <img src={animal.imageUrl} />
+        <p>{animal.latinName}</p>
+        <p>{animal.yearOfBirth}</p>
+        <p>{animal.medicine}</p>
+        <p>{animal.longDescription}</p>
+        <p>{animal.lastFed}</p>
+      </div>
+    </>
+  );
+};
