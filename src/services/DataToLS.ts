537433482fcbc671205e12f61d3d99adbc248743
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-05-05 10:48:05 +0200

    fix: clean up code

--- src/services/DataToLS.ts
+++ src/services/DataToLS.ts
@@ -1,24 +1,6 @@
 import { getAnimalData } from "./getAnimalData";
 import { IAnimal } from "../models/IAnimal";
 
-export function setToLS (animals: IAnimal[]) {
-
-    //let animals: IAnimal[] = [];
-    localStorage.setItem("animals", JSON.stringify(animals) || "[]");
-
-} 
-    
-
-    /*let animalsLS = localStorage.getItem("animals");
-
-    if (animalsLS) {
-        animals = JSON.parse(animalsLS);
-    } else {
-        await getAnimalData().then((response) => {
-            animals = response;
-            console.log(response);
-        }).catch((error) => {
-            console.log(error);
-        });
-    }
-    return animals;*/
+export function setToLS(animals: IAnimal[]) {
+  localStorage.setItem("animals", JSON.stringify(animals) || "[]");
+}
