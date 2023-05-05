42eb7b931ecf9ae0e20066d5df0142631fe12ba0
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-05-05 14:53:29 +0200

    feat: start with setting timestamp for lastFed

--- src/components/pages/AnimalView.tsx
+++ src/components/pages/AnimalView.tsx
@@ -5,6 +5,7 @@ import { getLSData } from "../../services/getLSData";
 import { IAnimal } from "../../models/IAnimal";
 import { useState } from "react";
 import { setToLS } from "../../services/DataToLS";
+import "../../styles/AnimalView.scss";
 
 export const AnimalView = () => {
   const animalsLS = getLSData();
@@ -16,6 +17,34 @@ export const AnimalView = () => {
   const clickedAnimal = animals.find(
     (animal) => animal.id.toString() === params.id
   );
+  /*
+  const updateTime = () => {
+    
+  }*/
+
+  const updateHungerStatus = () => {
+    animals.map((animal) => {
+      if (animal.id.toString() === params.id) {
+        // animal.lastFed = Date.now.toString();
+        let time = Date.now() - new Date(animal.lastFed).getTime();
+        console.log(time.toString());
+        console.log(animal.lastFed);
+      }
+      if (animal.isFed == true) {
+        //animal.lastFed = new Date().toUTCString();
+        animal.lastFed = new Date().toLocaleString();
+        console.log("animal was fed", animal.isFed);
+      }
+      let button = <button onClick={setAnimal}>Feed</button>;
+      if (animal.isFed == true) {
+        button = <button disabled>Feed</button>;
+      }
+      return <>{button}</>;
+    });
+
+    setAnimals([...animals]);
+    setToLS([...animals]);
+  };
 
   const setAnimal = () => {
     animals.map((animal) => {
@@ -29,6 +58,8 @@ export const AnimalView = () => {
     setToLS([...animals]);
   };
 
+  //let time = Date.now() - new Date(clickedAnimal.lastFed).getTime();
+
   if (clickedAnimal === undefined) {
     return <p>test</p>;
   } else {
@@ -37,6 +68,7 @@ export const AnimalView = () => {
         <Navbar></Navbar>
         <Animal {...clickedAnimal}></Animal>
         <button onClick={setAnimal}>Feed</button>
+        <button onClick={updateHungerStatus}>Test</button>
       </>
     );
   }
