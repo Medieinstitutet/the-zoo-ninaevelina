1038a6d38bb0a88fbe3b9cf20ae07cc6d738255c
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-05-05 10:43:23 +0200

    fix: update interface

--- src/models/IAnimal.ts
+++ src/models/IAnimal.ts
@@ -1,12 +1,12 @@
 export interface IAnimal {
-    id: number;
-    name: string;
-    latinName: string;
-    yearOfBirth: number;
-    shortDescription: string;
-    longDescription: string;
-    imageUrl: string;
-    medicine: string;
-    isFed: boolean;
-    lastFed: string;
+  id: number;
+  name: string;
+  latinName: string;
+  yearOfBirth: number;
+  shortDescription: string;
+  longDescription: string;
+  imageUrl: string;
+  medicine: string;
+  isFed: boolean;
+  lastFed: string;
 }
