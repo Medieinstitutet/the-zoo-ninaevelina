a711160c1c4769b0d44092dafe11bb4b30bf48ca
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-05-08 14:01:20 +0200

    (fix): update animal component

--- src/components/pages/Animal.tsx
+++ src/components/pages/Animal.tsx
@@ -5,19 +5,42 @@ import { IAnimal } from "../../models/IAnimal";
 import { useParams } from "react-router-dom";
 import { getLSData } from "../../services/getLSData";
 
-//  <div key={animal.id}>
-
 export const Animal = (animal: IAnimal) => {
   return (
     <>
-      <div>
+      <div className={animal.isFed ? "fed" : "hungry"}>
         <h1>{animal.name}</h1>
         <img src={animal.imageUrl} />
-        <p>{animal.latinName}</p>
-        <p>{animal.yearOfBirth}</p>
-        <p>{animal.medicine}</p>
-        <p>{animal.longDescription}</p>
-        <p>{animal.lastFed}</p>
+        <p>
+          <strong>
+            Latin name:<br></br>
+          </strong>{" "}
+          {animal.latinName}
+        </p>
+        <p>
+          <strong>
+            Year of Birth:<br></br>
+          </strong>{" "}
+          {animal.yearOfBirth}
+        </p>
+        <p>
+          <strong>
+            Medicine:<br></br>
+          </strong>{" "}
+          {animal.medicine}
+        </p>
+        <p>
+          <strong>
+            About:<br></br>
+          </strong>{" "}
+          {animal.longDescription}
+        </p>
+        <p>
+          <strong>
+            Last fed:<br></br>
+          </strong>{" "}
+          {animal.lastFed}
+        </p>
       </div>
     </>
   );
