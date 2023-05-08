004485b277659cc24721c79fcd4d99fef39aaf03
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-05-08 14:00:30 +0200

    (feat): add image to start page

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -5,6 +5,13 @@ export const Main = () => {
     <>
       <Navbar></Navbar>
       <h1>The Zoo</h1>
+      <div>
+        <img
+          src="https://media.istockphoto.com/id/543984390/photo/cut-out-giraffe-on-white-background.jpg?s=612x612&w=0&k=20&c=YVCEdJkdWgnCUkD1-dlVjUvFaU-AUIU2sGc_cHO5jes="
+          className="img-mainpage"
+          alt="picture of giraffe"
+        />
+      </div>
     </>
   );
 };
